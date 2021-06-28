a582ade8c1d5a6044bba2ee0a7cf3114adad9a01
shuliqi <shuliqi@360.cn>
2021-06-28 20:30:09 +0800

    query/hash

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>  
 <div>
-   <router-link to='/requireDetail/123'>详情页面</router-link> / 
+   <router-link to='/requireDetail/123?name=shuliqi&age=12#sex'>详情页面</router-link> / 
    <router-link to='/'>首页</router-link>
    <br/>
    <router-view></router-view>
 </div>
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-}
-</script>
--- src/components/requireDetail.vue
+++ src/components/requireDetail.vue
@@ -9,14 +9,14 @@ import { useRoute } from "vue-router";
 export default {
   setup () {
     const route = useRoute();
-    console.log("vue3：", route.params.requireId)
+    console.log("vue3：", route.params, route.query, route.hash)
     return {
       requireId: route.params.requireId
     }
   },
   created() {
   	// 获取动态路由的路径参数
-    console.log("vue3之前：", this.$route.params.requireId)
+    console.log("vue3之前：", this.$route.params,  this.$route.query, this.$route.hash)
   }
 }
 </script>
